6fec9ef1cfd3208ea641b644cf8f85959f833cb8
vzikratyi <vzikratyi@playtika.com>
2025-02-01 22:20:06 +0200

    Initial Commit

---
+++ raketno-nebezpechna-slobidka/raketno_nebezpechna_slobidkaApp.swift
@@ -0,0 +1,17 @@
+//
+//  raketno_nebezpechna_slobidkaApp.swift
+//  raketno-nebezpechna-slobidka
+//
+//  Created by Viktor Zikratyi on 01.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct raketno_nebezpechna_slobidkaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
