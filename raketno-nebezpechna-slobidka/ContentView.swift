5827b69aff73bc3c88fd7356f789fcec5181f5b0
vzikratyi <vzikratyi@playtika.com>
2025-02-02 19:35:16 +0200

    Init commit

--- raketno-nebezpechna-slobidka/ContentView.swift
+++ raketno-nebezpechna-slobidka/ContentView.swift
@@ -1,22 +1,47 @@
-//
-//  ContentView.swift
-//  raketno-nebezpechna-slobidka
-//
-//  Created by Viktor Zikratyi on 01.02.2025.
-//
 
 import SwiftUI
+import AVFoundation
+import UIKit
+import UserNotifications
 
 struct ContentView: View {
+    @State private var audioPlayer: AVAudioPlayer?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+                   Image(systemName: "speaker.wave.3.fill")
+                       .imageScale(.large)
+                       .foregroundStyle(.tint)
+                   Text("Playing Sound!")
+               }
         .padding()
+        .onAppear {
+                    setupAudio()
+                    startTimer()
+                }
     }
+    
+    func setupAudio() {
+        if let soundURL = Bundle.main.url(forResource: "alarm", withExtension: "wav") {
+            print("Sound file found at: \(soundURL)")
+            
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+                audioPlayer?.prepareToPlay()
+            } catch {
+                print("Error loading sound: \(error.localizedDescription)")
+            }
+        } else {
+            print("Sound file not found!")
+        }
+            
+        }
+    
+    func startTimer() {
+        Timer.scheduledTimer(withTimeInterval: 5.0, repeats: true) { _ in
+               audioPlayer?.play()
+           }
+       }
 }
 
 #Preview {
